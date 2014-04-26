24f120fb6d347d823303eee729c8f85c4d2c6624
DivicNikola <DivicNikola@gmail.com>
2014-04-26 18:28:57 +0000

    Ajde radi molim te.

---
+++ rs2-project/Assets/Scripts/InitializeWorld.cs
@@ -0,0 +1,164 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class InitializeWorld : MonoBehaviour {
+
+    private bool generated = false;
+    private int[,] mazeMatrix;
+    private bool sent = false;
+    int matrixSize = 0;
+
+	// Generate and render the world
+	void Start () {
+
+        if (PhotonNetwork.isMasterClient)
+        {
+            Debug.Log("I am the master client. Generating the maze.");
+
+            // If staticMaze variable is set generate a static maze from a given file
+            // procedurally generate a maze otherwise
+            if (ConfigManager.staticMaze)
+            {
+                mazeMatrix = GenerateWorld.GenerateMazeStatic(@"maze32.txt");
+                matrixSize = mazeMatrix.GetLength(0);
+            }
+            else
+            {
+                mazeMatrix = GenerateWorld.GenerateMaze();
+                matrixSize = mazeMatrix.GetLength(0);
+            }
+
+            Debug.Log("Rendering the maze.");
+            GenerateWorld.RenderMaze(mazeMatrix);
+            generated = true;
+        }
+        else
+        {
+            Debug.Log("I am not the master client, waiting for data to be recieved over the network.");
+        }
+        
+
+	}
+
+
+    #region Variables necessary for data transfer over the network
+
+    int rowsSent = 0;
+    int rowsRecieved = 0;
+   
+    List<int[]> rows= new List<int[]>();
+    bool sizeSent = false;
+
+    #endregion
+
+
+    #region Sending data over the network method 
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        
+        if (stream.isWriting) {
+            //  only the master client sends the matrix data
+            if (PhotonNetwork.isMasterClient && rowsSent<matrixSize)
+            {
+                // the size of the matrix is sent first
+                if (!sizeSent)
+                {
+                    Debug.Log("Sending: Matrix size = " + matrixSize);
+                    stream.SendNext(matrixSize);
+                    sizeSent = true;
+                }
+                    //send the rows
+                else
+                {
+                    stream.SendNext(getRow(rowsSent, mazeMatrix));
+                    rowsSent++;
+                }
+            }
+               
+            }
+            else 
+            {
+                if (!PhotonNetwork.isMasterClient)
+                {
+                    // read the size of the matrix
+                    if (matrixSize == 0)
+                    {
+                        matrixSize = (int)stream.ReceiveNext();
+                        Debug.Log("Matrix size = " + matrixSize);
+                    }
+                    // read the rows 
+                    else
+                    {
+
+                        int[] current = (int[])stream.ReceiveNext();
+
+                        rows.Add(current);
+                        rowsRecieved++;
+
+                        if (rowsRecieved == matrixSize)
+                        {
+                            Debug.Log("Recieved data. Rendering the world.");
+
+                            mazeMatrix = combineRows(rows);
+
+                            GenerateWorld.RenderMaze(mazeMatrix);
+                        }
+                    }
+
+                }
+            }   
+
+    }
+
+    #endregion
+
+
+    #region Support methods for network transfer
+
+    /// <summary>
+    /// Combine a list of int arrays into a two dimensional matrix
+    /// </summary>
+    /// <param name="rows"> List of rows </param>
+    /// <returns> A two dimensional integer matrix </returns>
+    private int[,] combineRows(List<int[]> rows)
+    {
+        int r = rows.Count;
+        int c = rows[0].Length; 
+
+        var matrix = new int[r, c];
+
+        for (int i = 0; i < r; i++)
+        {
+            for (int j = 0; j < c; j++)
+            {
+                matrix[i, j] = rows[i][j];
+            }
+        }
+
+            return matrix;
+    }
+
+
+    /// <summary>
+    /// Fetch one row from an integer matrix
+    /// </summary>
+    /// <param name="index"> Row index</param>
+    /// <param name="matrix"> Target matrix </param>
+    /// <returns> An array of integers representing a row </returns>
+    private int[] getRow(int index, int[,] matrix)
+    {
+        var result = new int[31];
+
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            result[j] = matrix[index, j];
+        }
+
+        return result;
+    }
+
+    #endregion
+
+}
---
+++ rs2-project/Assets/Scripts/Sprint.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Makes the character sprint when holding shift
+/// </summary>
+public class Sprint : MonoBehaviour
+{
+   
+    public float sprintSpeed;
+    private float walkSpeed;
+
+    private CharacterMotor motor;
+    private Transform tr;
+
+    void Start()
+    {
+        motor = GetComponent<CharacterMotor>();
+
+        //set walking speed to default character motor walking speed
+        walkSpeed = motor.movement.maxForwardSpeed;
+
+        tr = transform;
+    }
+
+    // check every frame
+    void FixedUpdate()
+    {
+        // default speed
+        float speed = walkSpeed;
+        
+        // if shift is pressed change the speed
+        if ((Input.GetKey("left shift") || Input.GetKey("right shift")) && motor.grounded)
+        {
+            speed = sprintSpeed;
+        }
+
+        // set the speed
+        motor.movement.maxForwardSpeed = speed; 
+    }
+}
