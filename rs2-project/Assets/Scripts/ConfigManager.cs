50edb9c50efd216d9cf6ac61207eac107e14f649
urosh3d <urosh3d@gmail.com>
2014-04-26 23:16:07 +0000

    Ubacen kontroler za DarkPrim-a, doradjena SpawnPlayer funkcija, lag za lampu delimicno implementiran

--- rs2-project/Assets/Scripts/ConfigManager.cs
+++ rs2-project/Assets/Scripts/ConfigManager.cs
@@ -4,9 +4,6 @@ using System.Collections;
 public class ConfigManager : MonoBehaviour
 {
 		static public bool gameCanEnd = false;
- 
 		static public bool staticMaze = false;
-
-		static public string firstGeneratedPlayer = "Prim";
-        static public bool canJump = true;
+		static public bool canJump = true;
 }
---
+++ rs2-project/Assets/Scripts/FlashlightLag.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlashlightLag : MonoBehaviour
+{
+		public Transform Camera;
+		
+		private Quaternion previousRotation;
+
+		// Use this for initialization
+		void Start ()
+		{
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{				
+				this.transform.rotation = Quaternion.Slerp (this.transform.rotation, Camera.transform.rotation, Time.deltaTime * 5.0f);
+		}
+}
