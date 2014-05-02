f5d2f37bec425f28f2d0ca30e8ff14370d5d59ca
urosh3d <urosh3d@gmail.com>
2014-05-02 18:16:14 +0000

    Implementiran lag lampe za kamerom, ispravljen bug sa prolaskom kamere kroz zidove, izmenjena hijerarhija objekata za Prima

---
+++ rs2-project/Assets/Scripts/FlashlightBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlashlightBehaviour : MonoBehaviour
+{
+		public Transform parent;
+		public float lag = 5.0f;
+		
+		private Transform camera;
+
+		// Use this for initialization
+		void Start ()
+		{
+				camera = parent.FindChild ("MainCamera");
+				if (!camera) 
+						Debug.Log ("FlashlightBehaviour: No camera object found on parent.");
+				
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{		
+				//follow parent
+				this.transform.position = parent.transform.position;
+
+				//sway with camera
+				this.transform.rotation = Quaternion.Slerp (this.transform.rotation, camera.transform.rotation, Time.deltaTime * lag);
+		}
+}
