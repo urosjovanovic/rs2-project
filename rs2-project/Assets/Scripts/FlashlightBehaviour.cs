abdfebd36a38cb20851add0102e177158c45325d
urosh3d <urosh3d@gmail.com>
2014-05-22 18:08:37 +0000

    Backup commit

--- rs2-project/Assets/Scripts/FlashlightBehaviour.cs
+++ rs2-project/Assets/Scripts/FlashlightBehaviour.cs
@@ -5,19 +5,17 @@ public class FlashlightBehaviour : MonoBehaviour
 {
 		public Transform parent;
 		public float lag = 5.0f;
-		public float fireRate = 0.5f;	
-		float coolDown = 0;
 
 		private Transform camera;
 
 		// Use this for initialization
 		void Start ()
-		{
-				camera = parent.FindChild ("MainCamera");
+        {
+            camera = parent.FindChild ("MainCamera");
 				if (!camera) 
 						Debug.Log ("FlashlightBehaviour: No camera object found on parent.");
-				
-		}
+
+        }
 	
 		// Update is called once per frame
 		void Update ()
@@ -28,32 +26,5 @@ public class FlashlightBehaviour : MonoBehaviour
 				//sway with camera
 				this.transform.rotation = Quaternion.Slerp (this.transform.rotation, camera.transform.rotation, Time.deltaTime * lag);
 
-				
-				//flashlight rays
-				if (this.gameObject.light.enabled) 
-				{
-					coolDown -= Time.deltaTime;
-					Fire ();
-				}
-		}
-
-		void Fire()
-		{
-				if (coolDown > 0)
-						return;
-
-				var fwd = transform.TransformDirection (Vector3.forward);
-				RaycastHit hit;
-			
-				if (Physics.Raycast (transform.position, fwd, out hit, 3)) 
-				{
-						if (hit.rigidbody!=null && hit.rigidbody.gameObject.tag == "DarkPrim")
-						{
-							Debug.Log ("There is something in front of the object!");
-							//hit.rigidbody.gameObject.GetComponent<CharacterController>().enabled=false;
-						}
-				}
-				
-				coolDown = fireRate;
 		}
 }
