ed1bad0ea03ae24c53577a5024af19c412c0e683
urosh3d <urosh3d@gmail.com>
2014-06-08 16:01:55 +0000

    Ubacen dummy sprite za DarkPrima

---
+++ rs2-project/Assets/Scripts/DarkPrim2DSpriteBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DarkPrim2DSpriteBehaviour : MonoBehaviour {
+
+    private Transform target;
+    /// <summary>
+    /// The object which should 2D sprite be oriented at.
+    /// </summary>
+    public Transform Target
+    {
+        get { return target; }
+        set { target = value; }
+    }
+
+	// Use this for initialization
+	void Start () {
+        target = GameObject.FindGameObjectWithTag("Prim").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(target!=null)
+        {
+            this.transform.LookAt(target.position);
+        }
+	}
+}
