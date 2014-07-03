6bd43a16cba3d83e8ded2703367b98d4f42c3ee7
jupikearilje@gmail.com <jupikearilje@gmail.com@9201cf2d-efc5-ebe5-0202-3243bb312d2a>
2014-07-03 19:55:13 +0000

    Popravka predhodnog + centriran text Loading...

---
+++ trunk/rs2-project/Assets/Scripts/LoadingScreenSettings.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadingScreenSettings : MonoBehaviour 
+{
+    private int screenWidth;
+    private int screenHeight;
+    private GUIText loadingText;
+    private float timeDelay = 1.0f;
+    private float timeRemaining;
+    private int numberOfDots = 0;
+
+	// Use this for initialization
+	void Start () 
+    {
+        timeRemaining = timeDelay;
+
+        screenWidth = Screen.height;
+        screenHeight = Screen.width;
+        
+        loadingText = GetComponentInParent<GUIText>();
+        if (loadingText != null)
+            transform.position = new Vector2(0.5f, 0.5f);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        timeRemaining -= Time.deltaTime;
+
+        if(screenWidth != Screen.height || screenHeight != Screen.width)
+        {
+            if (loadingText != null)
+                transform.position = new Vector2(0.5f, 0.5f);
+        }
+        else if(timeRemaining < 0)
+        {
+            if (loadingText != null)
+            {
+                numberOfDots = (numberOfDots + 1) % 4;
+
+                switch(numberOfDots)
+                {
+                    case 0:
+                        loadingText.text = "Loading";
+                        break;
+                    case 1:
+                        loadingText.text = "Loading .";
+                        break;
+                    case 2:
+                        loadingText.text = "Loading . .";
+                        break;
+                    case 3:
+                        loadingText.text = "Loading . . .";
+                        break;
+                }
+            }
+
+            timeRemaining = timeDelay;
+        }
+	}
+}
